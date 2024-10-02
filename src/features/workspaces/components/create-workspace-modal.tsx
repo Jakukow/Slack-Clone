22426e21bde1d9681f50fd296b5592a210ff6dab
jakukow <jakubkowolik00@gmail.com>
2024-10-02 13:29:39 +0200

    Workspace added

---
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -0,0 +1,57 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useCreateWorkspace } from "../api/use-create-workspaces";
+import { useState } from "react";
+
+export const CreateWorkspaceModal = () => {
+  const [open, setOpen] = useCreateWorkspaceModal();
+  const [name, setName] = useState("");
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const { mutate, isPending } = useCreateWorkspace();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate(
+      { name },
+      {
+        onSuccess(data) {
+          console.log(data);
+        },
+      }
+    );
+  };
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a workspace</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled={isPending}
+            required
+            autoFocus
+            minLength={3}
+            placeholder="Workspace name e.g 'Work', 'Personal', 'Home'"
+          />
+          <div className="flex justify-end">
+            <Button disabled={isPending}>Create</Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};

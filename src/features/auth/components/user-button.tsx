6f421f947ed8a8cc9e76277fcce982f47c4401f6
jakukow <jakubkowolik00@gmail.com>
2024-10-07 23:58:44 +0200

    Joining implemented

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -35,7 +35,12 @@ export const UserButton = () => {
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="center" side="right" className="w-60">
-        <DropdownMenuItem onClick={() => signOut()} className="h-10">
+        <DropdownMenuItem
+          onClick={() => {
+            signOut();
+          }}
+          className="h-10"
+        >
           <LogOut className="size-4 mr-2" /> Log out
         </DropdownMenuItem>
       </DropdownMenuContent>
